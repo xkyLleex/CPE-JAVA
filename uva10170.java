427dd981567df1fdcebb05180d8ce6449e73423e
kyL_lee <leekwanyao710@gmail.com>
2019-11-18 14:28:25 +0800

    Update uva10170.java

--- uva10170.java
+++ uva10170.java
@@ -4,14 +4,12 @@ public class uva10170 {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		while(true) {//on cpe need use in.hasNext()
-			int number = in.nextInt();
+			long number = in.nextLong();
 			if(number == 0)break;//jump out
-			int day = in.nextInt(),countday = 0;
-			while(true) {
-				for(int i=0;i<number;i++) 
-					countday += 1;
-				if(countday >= day)break;
+			long day = in.nextLong(),countday = number;
+			while(countday < day){
 				number++;
+				countday += number;
 			}
 			System.out.println(number);
 		}
