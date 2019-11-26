9fde70b5dc5a8a479ada1b6271ba935743597b55
kyL_lee <leekwanyao710@gmail.com>
2019-11-26 21:05:35 +0800

    Add files via upload

---
+++ uva948.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class uva948 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int count = in.nextInt();
+		for(int casec = 0;casec < count;casec++){
+			int num = in.nextInt();
+			ArrayList<Integer> fibs = new ArrayList<Integer>();
+			fibs.add(1);fibs.add(2);
+			int n = 0,sum = 1;
+			while(num > sum){
+				sum += fibs.get(n+1);
+				fibs.add(fibs.get(n) + fibs.get((n++)+1));
+			}
+			String outstr = "";
+			for(int i=fibs.size()-1;i>=0;i--){
+				if(fibs.get(i) <= num){
+					outstr += "1";
+					num -= fibs.get(i);
+				}else
+					outstr += (outstr != "")?"0":"";
+			}
+			System.out.println(outstr + " (fib)");
+		}
+		in.close();
+	}
+}
