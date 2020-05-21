749c8856ffc4dc36c62662e0f1ad5617f24d75f2
kyL_lee <leekwanyao710@gmail.com>
2020-05-21 14:09:23 +0800

    Update uva10252.java

--- uva10252.java
+++ uva10252.java
@@ -10,8 +10,6 @@ public class uva10252 {
 			if(text1.charAt(0) == '0' && text2.charAt(0) == '0')break;//jump out
 			int count1[] = new int[26];
 			int count2[] = new int[26];
-			Arrays.fill(count1, 0);
-			Arrays.fill(count2, 0);
 			for(int i=0;i<text1.length();i++){
 				if(text1.charAt(i)>96&&text1.charAt(i)<123)
 					count1[text1.charAt(i)-97] += 1; 
