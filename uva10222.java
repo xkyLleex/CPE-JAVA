989bc7e1ad02a35f7a8af76338a5dc6776e85c40
kyL_lee <leekwanyao710@gmail.com>
2020-05-21 14:37:08 +0800

    Update uva10222.java

--- uva10222.java
+++ uva10222.java
@@ -3,27 +3,20 @@ import java.util.Scanner;
 public class uva10222 {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		String keyboard = "`1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./";//多一個\是因為編譯問題
-		while(true){//on cpe need use in.hasNext()
-			String word = "";
+		String keyboard = "`1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./";
+		while(in.hasNext()){
 			String code = in.nextLine().toLowerCase();
-			if(code.charAt(0) == '0')break;//jump out
 			for(int i=0;i<code.length();i++){
 				for(int j=0;j<keyboard.length();j++){
 					if(keyboard.charAt(j) == code.charAt(i)){
-						if(j-2<0)
-							word += keyboard.charAt(j-2+keyboard.length());
-						else
-							word += keyboard.charAt(j-2);
+						System.out.print(keyboard.charAt(j-2));
 						break;
-					}
-					if(j == keyboard.length()-1)
-						word += code.charAt(i);
+					}else if(j == keyboard.length()-1)
+						System.out.print(code.charAt(i));
 				}
 			}
-			System.out.println(word);
+			System.out.println();
 		}
 		in.close();
 	}
-
 }
