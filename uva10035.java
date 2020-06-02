10693c59aa74481035662912af11179a0df9259b
kyL_lee <leekwanyao710@gmail.com>
2020-06-02 16:02:16 +0800

    Update uva10035.java

--- uva10035.java
+++ uva10035.java
@@ -3,29 +3,24 @@ import java.util.*;
 public class uva10035{
     public static void main(String[] args) {
         try{
-            int a,b,sum,carry;
             Scanner in=new Scanner(System.in);
             while(true){
-            	a = in.nextInt();b = in.nextInt();
-            	if(a==0&&b==0)break;
-            	sum = 0;carry = 0;
-            	while(true){
-            		int alastnum = a%10;int blastnum = b%10;
-            		if(alastnum + blastnum + carry > 9){
-            			sum++;
+            	int a=in.nextInt(),b=in.nextInt();
+            	if(a == 0 && b == 0)break;
+            	int carry = 0,carrycount = 0;
+            	while(a != 0 || b != 0){
+            		int amod = a % 10,bmod = b % 10;
+            		if((carry + amod + bmod) > 9){
             			carry = 1;
+            			carrycount++;
             		}else{
             			carry = 0;
             		}
             		a /= 10;b /= 10;
-            		if(a==0&&b==0)break;
             	}
-            	if(sum==0)
-            		System.out.println("No carry operation.");
-            	else if(sum==1)
-            		System.out.println("1 carry operation.");
-            	else
-            		System.out.println(sum + " carry operations.");
+            	if(carrycount == 0){System.out.println("No carry operation.");}
+            	else if(carrycount == 1){System.out.println("1 carry operation.");}
+            	else{System.out.println(carrycount + " carry operations.");}
             }
             in.close();
         }catch(Exception e){
