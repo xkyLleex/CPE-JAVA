4f53d9c6183e227c35b41947c7bfd4295e8f3132
kyL_lee <leekwanyao710@gmail.com>
2020-06-02 15:56:52 +0800

    Update uva11321.java

--- uva11321.java
+++ uva11321.java
@@ -3,43 +3,31 @@ import java.util.*;
 public class uva11321{
     public static void main(String[] args) {
         try{
-            int n,m;
-            Scanner in=new Scanner(System.in);
-            while(true){
-            	n=in.nextInt();m=in.nextInt();
-            	if((n==0)&&(m==0))break;
-            	System.out.println(n + " " + m);
-            	int intlist[] = new int[n];
-            	for(int i=0;i<n;i++)
-            		intlist[i] = in.nextInt();
-            	for(int i=0;i<n-1;i++){
-            		if(intlist[i] % m > intlist[i+1] % m){
-            			i = listchange(i,intlist);
-            		}else if(intlist[i] % m == intlist[i+1] % m){
-            			if((intlist[i] % 2==0)&&(intlist[i+1] % 2==0)){//even even
-            				if(intlist[i] > intlist[i+1])
-            					i = listchange(i,intlist);
-            			}else if((intlist[i] % 2!=0)&&(intlist[i+1] % 2!=0)){//odd odd
-            				if(intlist[i] < intlist[i+1])
-            					i = listchange(i,intlist);
-            			}else{
-            				if(intlist[i] % 2==0)//even,odd to odd,even
-            					i = listchange(i,intlist);
-            			}
-            		}
-            	}
-            	for(int i=0;i<n;i++)
-            		System.out.println(intlist[i]);
-            }
-            in.close();
+            Scanner in = new Scanner(System.in);
+           	while(true){
+           		int numlist []= new int[in.nextInt()],modnum = in.nextInt();
+           		System.out.println(numlist.length + " " + modnum);
+           		if(numlist.length == 0 && modnum == 0)break;
+           		int minmod = 0;
+           		for(int i=0;i<numlist.length;i++){
+           			numlist[i] = in.nextInt();
+           			minmod = (numlist[i]%modnum<minmod)?numlist[i]%modnum:minmod;
+           		}
+           		Arrays.sort(numlist);
+           		for(int j=minmod;j<modnum;j++){
+	           		for(int i=numlist.length-1;i>=0;i--){//odd
+	           			if(numlist[i] % modnum == j && numlist[i] % 2 != 0)
+	           				System.out.println(numlist[i]);
+	           		}
+	           		for(int i=0;i<numlist.length;i++){//even
+	           			if(numlist[i] % modnum == j && numlist[i] % 2 == 0)
+	           				System.out.println(numlist[i]);
+	           		}
+	           	}
+           	}
+           	in.close();
         }catch(Exception e){
-            System.out.println("Input error!");
+            System.out.println(e);
         }
-    }
-    public static int listchange(int a,int numlist[]){
-    	int change = numlist[a];
-    	numlist[a] = numlist[a+1];
-    	numlist[a+1] = change;
-    	return -1;
     }
 };
