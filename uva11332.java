02a87c75e106f3933354df5ec1aee806436accc7
kyL_lee <leekwanyao710@gmail.com>
2020-06-03 19:06:03 +0800

    Update uva11332.java

--- uva11332.java
+++ uva11332.java
@@ -2,20 +2,17 @@ import java.util.Scanner;
 
 public class uva11332 {
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+		Scanner in=new Scanner(System.in);
 		while(true){
-			String num = in.nextLine();
-			if(num.charAt(0) == '0')break;
-			while(true){
+			String num = in.next();
+			if(num.equals("0"))break;
+			while(num.length() != 1){
 				int sum = 0;
 				for(int i=0;i<num.length();i++)
-					sum += num.charAt(i)-48;
-				if(sum / 10 == 0){
-					System.out.println(sum);
-					break;
-				}else
-				 	num = Integer.toString(sum);
+					sum += num.charAt(i) - '0';
+				num = Integer.toString(sum);
 			}
+			System.out.println(num);
 		}
 		in.close();
 	}
