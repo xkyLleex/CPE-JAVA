cf8424c631207e70f2ffe45200f1532db211269b
kyL_lee <leekwanyao710@gmail.com>
2020-06-03 17:54:29 +0800

    Update uva10008.java

--- uva10008.java
+++ uva10008.java
@@ -2,34 +2,25 @@ import java.util.Scanner;
 
 public class uva10008 {
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		String String_line[] = new String[in.nextInt()];
-		in.nextLine();
-		for(int i=0;i<String_line.length;i++)
-			String_line[i] = in.nextLine();
-		int charcount[] = new int[26];//a-z 0-25
-		int bignum = 0;
-		for(int i=0;i<String_line.length;i++){
-			for(int j=0;j<String_line[i].length();j++){
-				if(String_line[i].charAt(j) > 64 && String_line[i].charAt(j) < 91){
-					charcount[String_line[i].charAt(j)-65] += 1;
-					if(bignum < charcount[String_line[i].charAt(j)-65])
-						bignum = charcount[String_line[i].charAt(j)-65];
-				}else if(String_line[i].charAt(j) > 96 && String_line[i].charAt(j) < 123){
-					charcount[String_line[i].charAt(j)-97] += 1;
-					if(bignum < charcount[String_line[i].charAt(j)-97])
-						bignum = charcount[String_line[i].charAt(j)-97];
-				}else
-					continue;
-			}
-		}
-		while(bignum != 0){
-			for(int i=0;i<26;i++){
-				if(bignum == charcount[i])
-					System.out.println((char)(65+i) + " " +bignum);
-			}
-			bignum--;
-		}
-		in.close();
+        Scanner in=new Scanner(System.in);
+        int cases = in.nextInt();in.nextLine();
+        int bet[] = new int[26];
+        while((cases--)>0){
+            String str = in.nextLine();
+            for(int i=0;i<str.length();i++){
+                if(str.charAt(i) > 64 && str.charAt(i) < 91)
+                    bet[str.charAt(i)-65]++;
+                else if(str.charAt(i) > 96 && str.charAt(i) < 123)
+                    bet[str.charAt(i)-97]++;
+            }
+        }
+        int max = 0;
+        for(int i=0;i<26;i++)max=(bet[i] > max)?bet[i]:max;
+        while((max--)>0){
+            for(int i=0;i<26;i++)
+                if(bet[i] == max+1)
+                    System.out.println((char)(65+i) + " " + (max+1));
+        }
+        in.close();
 	}
 }
