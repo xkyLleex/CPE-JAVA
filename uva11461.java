021f9f2d2c77cb48a6a3dcc5744dd965c02e2bb5
kyL_lee <leekwanyao710@gmail.com>
2020-06-08 19:16:35 +0800

    Update uva11461.java

--- uva11461.java
+++ uva11461.java
@@ -3,17 +3,9 @@ import java.util.Scanner;
 public class uva11461 {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		while(true){
-			int a = in.nextInt(),b = in.nextInt(),sum = 0;
-			if(a==0 && b==0)break;	
-			for(int i=a;i<=b;i++){
-				if(Math.sqrt(i) % 1 != 0)
-					continue;
-				else
-					sum++;
-			}
-			System.out.println(sum);
-		}
+	    int a,b;
+	    while((a = in.nextInt()) != 0 && (b = in.nextInt()) != 0)
+			System.out.println((int)(Math.sqrt(b) - Math.ceil(Math.sqrt(a))) + 1);
 		in.close();
 	}
 }
